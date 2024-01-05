e8f8dc9c8bf10701822a3f745c759f9519ceb516
Saipoojitha <125653068+Poojitha62@users.noreply.github.com>
2024-01-05 23:15:33 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,14 @@
+ class Student{
+    id=34;
+    name="chandu";
+    constructor(id,name)
+    {
+        this.id=id||this.id;
+        this.name=name|| this.name;
+    }
+   
+}
+
+
+let S1=new Student()
+console.log(S1);
